7cf0b518145c0947219b07fffa56d2a154bb85d5
ZhangYang <yrzhang@thoughtworks.com>
2019-07-03 21:25:51 +0800

    add

--- buildscript/build.bash
+++ buildscript/build.bash
@@ -7,4 +7,4 @@ VERSION=${Build_Id:-'DEV'}
 REPOSITORY=restairline.azurecr.io/restairline
 
 
-docker build --build-arg Version=$VERSION -f ./src/RestAirline.Api/Dockerfile ./src --tag $REPOSITORY:$VERSION --tag $REPOSITORY:latest 
+docker build --build-arg Version=1 -f ./src/RestAirline.Api/Dockerfile ./src --tag $REPOSITORY:$VERSION --tag $REPOSITORY:latest 
