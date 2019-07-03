439abf7d33b185112604d95314f9d7dc4e76c7fb
ZhangYang <yrzhang@thoughtworks.com>
2019-07-03 20:50:49 +0800

    fix script

--- buildscript/build.bash
+++ buildscript/build.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -ex
 
-BASEDIR=$(dirname "$0")
+# BASEDIR=$(dirname "$0")
+
 VERSION=${Build_Id:-'DEV'}
 REPOSITORY='restairline.azurecr.io'
 
 
-docker build -f $BASEDIR/../src/RestAirline.Api/Dockerfile $BASEDIR/../src --tag $REPOSITORY:$VERSION --tag $REPOSITORY:latest 
+docker build -f ./../src/RestAirline.Api/Dockerfile ../src --tag $REPOSITORY:$VERSION --tag $REPOSITORY:latest 
