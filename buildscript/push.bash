3f21497532473ff55bb66f4d8fac245c6dd766e0
ZhangYang <yrzhang@thoughtworks.com>
2019-07-04 21:14:12 +0800

    add push.bash

---
+++ buildscript/push.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+VERSION=${BUILD_BUILDID:-'dev'}
+REPOSITORY=restairline.azurecr.io/restairline
+
+# docker login
+
+docker push $REPOSITORY::$VERSION
+docker push $REPOSITORY:latest
+
+echo "Cleaning docker images..."
+docker image ls | grep jedis | awk '{print $1":"$2}' | xargs docker rmi
+docker system prune -f
