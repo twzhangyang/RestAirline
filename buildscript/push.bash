333ca7dbdfd2ff642b4ca853a7ddc1b79e30bce1
ZhangYang <yrzhang@thoughtworks.com>
2019-07-04 21:19:12 +0800

    fix

--- buildscript/push.bash
+++ buildscript/push.bash
@@ -10,5 +10,5 @@ docker push $REPOSITORY::$VERSION
 docker push $REPOSITORY:latest
 
 echo "Cleaning docker images..."
-docker image ls | grep jedis | awk '{print $1":"$2}' | xargs docker rmi
+docker image ls | grep restairline | awk '{print $1":"$2}' | xargs docker rmi
 docker system prune -f
