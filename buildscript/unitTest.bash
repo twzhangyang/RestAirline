9790bea64f80db4991e5bce8b7bc37775db8f8bf
ZhangYang <yrzhang@thoughtworks.com>
2019-07-04 10:23:05 +0800

    fix

--- buildscript/unitTest.bash
+++ buildscript/unitTest.bash
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-# PROJECT_BASE_DIR=$(pwd)/$(dirname $0)/..
-
-for PROJECT in $(ls -1 ./tests | grep -e RestAiline | grep -v TestsHelper)
+for PROJECT in $(ls -1 ./tests | grep -e RestAirline | grep -v TestsHelper)
 do
   docker run --rm microsoft/dotnet:2.2.103-sdk dotnet test tests/$PROJECT -v minimal
 done
