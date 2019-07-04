556d6b6d9f9020a8ed71f407b3a2ef561460c141
ZhangYang <yrzhang@thoughtworks.com>
2019-07-04 13:25:57 +0800

    fix

--- buildscript/build.bash
+++ buildscript/build.bash
@@ -6,4 +6,4 @@ LABEL=${BUILD_BUILDNUMBER:-'1'}
 
 REPOSITORY=restairline.azurecr.io/restairline
 
-docker build --build-arg Version=$VERSION -f ./src/RestAirline.Api/Dockerfile ./src --tag $REPOSITORY:$VERSION --tag $REPOSITORY:latest
+docker build --build-arg Version=$LABEL -f ./src/RestAirline.Api/Dockerfile ./src --tag $REPOSITORY:$VERSION --tag $REPOSITORY:latest
--- buildscript/unitTest.bash
+++ buildscript/unitTest.bash
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -ex
-cd $(dirname $0)/..
+#cd $(dirname $0)/..
+FOLDER=${BUILD_ARTIFACTSTAGINGDIRECTORY:-'$(pwd)'}
 
 for PROJECT in $(ls -1 ./tests | grep -e RestAirline | grep -v TestsHelper)
 do
-  docker run --rm -v $(pwd):/project -w /project microsoft/dotnet:2.2.103-sdk dotnet test tests/$PROJECT -v minimal
+  docker run --rm -v $FOLDER:/project -w /project microsoft/dotnet:2.2.103-sdk dotnet test tests/$PROJECT -v minimal
 done
