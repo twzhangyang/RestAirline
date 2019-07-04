7f19a2087a44cc7ac8fc1367649b5e4e4803fc91
ZhangYang <yrzhang@thoughtworks.com>
2019-07-04 11:23:21 +0800

    ut

--- buildscript/unitTest.bash
+++ buildscript/unitTest.bash
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -ex
+cd $(dirname $0)/..
 
 for PROJECT in $(ls -1 ./tests | grep -e RestAirline | grep -v TestsHelper)
 do
-  docker run --rm microsoft/dotnet:2.2.103-sdk dotnet test tests/$PROJECT -v minimal
+  docker run --rm -v $(pwd):/project -w /project microsoft/dotnet:2.2.103-sdk dotnet test tests/$PROJECT -v minimal
 done
