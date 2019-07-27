1a2a28db5473af5940615c59e030eefe78f9774c
ZhangYang <yrzhang@thoughtworks.com>
2019-07-27 19:22:32 +0800

    fix test

--- tests/RestAirline.CommandHandlers.Tests/Journey/SelectJourneysCommandHandlerTests.cs
+++ tests/RestAirline.CommandHandlers.Tests/Journey/SelectJourneysCommandHandlerTests.cs
@@ -21,7 +21,12 @@ namespace RestAirline.CommandHandlers.Tests.Journey
         public async Task WhenSendSelectJourneysCommandShouldAddJourneys()
         {
             //Arrange
-         
+            var journeys = new JourneysBuilder().BuildJourneys();
+            var selectJourneysCommand = new SelectJourneysCommand(_bookingId, journeys);
+            
+            //Act
+            await CommandBus.PublishAsync(selectJourneysCommand, CancellationToken.None);
+
             //Assert
             var booking = await AggregateStore.LoadAsync<Booking, BookingId>(_bookingId, CancellationToken.None);
             booking.Journeys.Should().NotBeEmpty();
