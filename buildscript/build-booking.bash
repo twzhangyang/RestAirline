1b6a96c7b37b5bf80f08acaaf53517c3ff27dfc2
yrzhang <yrzhang@thoughtworks.com>
2020-02-01 00:41:00 +0800

    play with docker file with .net 3.1, still not work

--- buildscript/build-booking.bash
+++ buildscript/build-booking.bash
@@ -8,10 +8,10 @@ LABEL=${BUILD_BUILDID:-'1'}
 
 REPOSITORY=restairline.azurecr.io/restairline-booking
 
-docker build --build-arg Version=$LABEL -f ./src/Booking/RestAirline.Booking.Api/Dockerfile ./src --tag $REPOSITORY:$VERSION --tag $REPOSITORY:latest
+docker build --build-arg Version=$LABEL -f ./src/Booking/RestAirline.Booking.Api/Dockerfile ./src --tag $REPOSITORY:$VERSION
 
 echo "Cleaning docker images..."
-docker image ls | grep $REPOSITORY | awk '{print $1":"$2}' | xargs docker rmi
+# docker image ls | grep $REPOSITORY | awk '{print $1":"$2}' | xargs docker rmi
 docker system prune -f
 
 
