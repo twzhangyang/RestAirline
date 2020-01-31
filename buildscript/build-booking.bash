cee6589100fe11b5e9797e4d3ab7b11d5b52a65e
yrzhang <yrzhang@thoughtworks.com>
2020-01-31 23:57:32 +0800

    fix docker file but still blocked by dotnet restore...

--- buildscript/build-booking.bash
+++ buildscript/build-booking.bash
@@ -13,3 +13,5 @@ docker build --build-arg Version=$LABEL -f ./src/Booking/RestAirline.Booking.Api
 echo "Cleaning docker images..."
 docker image ls | grep $REPOSITORY | awk '{print $1":"$2}' | xargs docker rmi
 docker system prune -f
+
+
