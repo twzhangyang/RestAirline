f1d21eb2e24cb6accc4883fbfa04be0b4d50a423
yang <yrzhang@thoughtworks.com>
2020-05-17 12:52:40 +0800

    running tests for booking

--- buildscript/unitTest-booking.sh
+++ buildscript/unitTest-booking.sh
@@ -6,5 +6,5 @@ FOLDER="./tests/Booking"
 for PROJECT in $(ls -1 $FOLDER | grep -e RestAirline | grep -v TestsHelper | grep -v Elasticsearch | grep -v MassTransit)
 do
   echo $PROJECT
-  # docker run --rm -v $FOLDER:/project -w /project mcr.microsoft.com/dotnet/core/sdk:3.1 dotnet test $FOLDER/$PROJECT -v minimal
+  docker run --rm -v $FOLDER:/project -w /project mcr.microsoft.com/dotnet/core/sdk:3.1 dotnet test $FOLDER/$PROJECT -v minimal
 done
